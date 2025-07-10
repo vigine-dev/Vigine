6bcde1330fe0f0a69df68133fb0eb5d879f52083
Vitalii Hromko <vitalii.hromko.dn@renesas.com>
2025-07-10 23:13:17 +0300

    Create interface methods for working with components for the Entity interface methods for work with components for Entity(#44)

--- include/vigine/ecs/abstractsystem.h
+++ include/vigine/ecs/abstractsystem.h
@@ -10,6 +10,8 @@ namespace vigine
 using SystemId   = std::string;
 using SystemName = std::string;
 
+class Entity;
+
 class AbstractSystem : public EntityBindingHost
 {
   public:
@@ -17,6 +19,10 @@ class AbstractSystem : public EntityBindingHost
 
     virtual SystemId id() const = 0;
     SystemName name();
+
+    virtual bool hasComponents(Entity *entity) const = 0;
+    virtual void createComponents(Entity *entity)    = 0;
+    virtual void destroyComponents(Entity *entity)   = 0;
 
   protected:
     AbstractSystem(const SystemName &name);
