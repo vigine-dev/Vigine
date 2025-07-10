41822cd12974cd08575386887cb97572c54b23b7
Vitalii Hromko <vitalii.hromko.dn@renesas.com>
2025-07-10 22:01:32 +0300

    Create system in the context(#44)

--- include/vigine/context.h
+++ include/vigine/context.h
@@ -1,5 +1,6 @@
 #pragma conce
 
+#include "vigine/ecs/abstractsystem.h"
 #include <vigine/abstractservice.h>
 
 #include <memory>
@@ -14,19 +15,23 @@ enum class Property;
 class EntityManager;
 
 using ServiceInstancesContainer = std::vector<std::pair<const ServiceName, AbstractServiceUPtr>>;
+using SystemInstancesContainer  = std::vector<std::pair<const SystemId, AbstractSystemUPtr>>;
 
 class Context
 {
   public:
     AbstractService *service(const ServiceId id, const ServiceName name, const Property property);
+    AbstractSystem *system(const SystemId id, const SystemName name, const Property property);
     EntityManager *entityManager() const;
 
   private:
     Context(EntityManager *entityManager);
     AbstractServiceUPtr createService(const ServiceId &id, const ServiceName &name);
+    AbstractSystemUPtr createSystem(const SystemId &id, const SystemName &name);
 
   private:
     std::unordered_map<ServiceId, ServiceInstancesContainer> _services;
+    std::unordered_map<SystemId, SystemInstancesContainer> _systems;
     EntityManager *_entityManager{nullptr};
 
     friend class Engine;
--- src/context.cpp
+++ src/context.cpp
@@ -1,11 +1,63 @@
-#include <vigine/context.h>
-#include <vigine/property.h>
-#include <vigine/service/databaseservice.h>
+#include "vigine/context.h"
+
+#include "vigine/ecs/postgresqlsystem.h"
+#include "vigine/property.h"
+#include "vigine/service/databaseservice.h"
 
 #include <algorithm>
 #include <utility>
 
+vigine::AbstractSystem *vigine::Context::system(const SystemId id, const SystemName name,
+                                                const Property property)
+{
+    AbstractSystem *retVal{nullptr};
+
+    switch (property)
+        {
+        case Property::New:
+            {
+                auto system = createSystem(id, name);
+                if (!system)
+                    return retVal;
+
+                retVal = system.get();
+                _systems[id].emplace_back(name, std::move(system));
+            }
+
+            break;
+        case Property::Exist:
+            {
+                if (!_systems.contains(id))
+                    return retVal;
+
+                SystemInstancesContainer &systemContainer = _systems[id];
+                auto it = std::find_if(systemContainer.begin(), systemContainer.end(),
+                                       [&name](const auto &item) { return item.first == name; });
+
+                if (it != systemContainer.end())
+                    retVal = it->second.get();
+            }
+            break;
+        default:
+            break;
+        }
+
+    return retVal;
+}
+
 vigine::Context::Context(EntityManager *entityManager) { _entityManager = entityManager; }
+
+vigine::AbstractSystemUPtr vigine::Context::createSystem(const SystemId &id, const SystemName &name)
+{
+    if (id == "PostgreSQL")
+        {
+            auto postgreSQLSystem = std::make_unique<vigine::PostgreSQLSystem>(name);
+
+            return std::move(postgreSQLSystem);
+        }
+
+    return nullptr;
+}
 
 vigine::EntityManager *vigine::Context::entityManager() const { return _entityManager; }
 
@@ -50,7 +102,6 @@ vigine::AbstractService *vigine::Context::service(const ServiceId id, const Serv
 vigine::AbstractServiceUPtr vigine::Context::createService(const ServiceId &id,
                                                            const ServiceName &name)
 {
-
     if (id == "Database")
         {
 
