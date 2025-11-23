54b10791fe311a268ccaeb460320fa6a86d6bd77
Vitalii Hromko <vitalii.hromko.dn@renesas.com>
2025-11-23 14:43:43 +0200

    Add RenderSystem as memeber of the GraphicsService(#59)

--- src/service/graphicsservice.cpp
+++ src/service/graphicsservice.cpp
@@ -1,23 +1,16 @@
 #include "vigine/service/graphicsservice.h"
 
+#include "vigine/ecs/render/rendersystem.h"
+
 vigine::graphics::GraphicsService::GraphicsService(const Name &name) : AbstractService(name) {}
 
-void vigine::graphics::GraphicsService::contextChanged()
-{
-
-}
+void vigine::graphics::GraphicsService::contextChanged() {}
 
 void vigine::graphics::GraphicsService::entityBound()
 {
-    //Entity *ent = getBoundEntity();
-
-
-
+    // Entity *ent = getBoundEntity();
 }
 
-void vigine::graphics::GraphicsService::entityUnbound()
-{
-
-}
+void vigine::graphics::GraphicsService::entityUnbound() {}
 
 vigine::ServiceId vigine::graphics::GraphicsService::id() const { return "Graphics"; }
