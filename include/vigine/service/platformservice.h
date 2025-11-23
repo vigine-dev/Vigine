f987779a74f987d6243d00b1f8dad57d5c5fc60b
Vitalii Hromko <vitalii.hromko.dn@renesas.com>
2025-11-23 14:41:19 +0200

    Add WindowSystem as memeber of the PlatformService(#59)

--- include/vigine/service/platformservice.h
+++ include/vigine/service/platformservice.h
@@ -7,16 +7,22 @@ namespace vigine
 {
 namespace platform
 {
+class WindowSystem;
+
 class PlatformService : public AbstractService
 {
   public:
     PlatformService(const Name &name);
+    ~PlatformService() override;
 
     ServiceId id() const override;
 
   protected:
     void contextChanged() override;
     void entityBound() override;
+
+  private:
+    WindowSystem *_windowSystem{nullptr};
 };
 
 BUILD_SMART_PTR(PlatformService);
--- src/service/platformservice.cpp
+++ src/service/platformservice.cpp
@@ -1,8 +1,12 @@
 #include "vigine/service/platformservice.h"
 
+#include "vigine/ecs/platform/windowsystem.h"
+
 using namespace vigine::platform;
 
 PlatformService::PlatformService(const Name &name) : AbstractService(name) {}
+
+PlatformService::~PlatformService() = default;
 
 void PlatformService::contextChanged() {}
 
