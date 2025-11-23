e86b7d6ed7fbb76e30c7c838998b2690b23af86c
Vitalii Hromko <vitalii.hromko.dn@renesas.com>
2025-11-23 13:34:35 +0200

    Check what operation system(#59)

--- include/vigine/base/info.h
+++ include/vigine/base/info.h
@@ -26,5 +26,36 @@ constexpr BuildType buildType()
             return BuildType::Unknown;
         }
 }
+
+namespace platform
+{
+enum class Type
+{
+    Windows,
+    Linux,
+    MacOS,
+    Unknown
+};
+
+constexpr Type currentPlatform()
+{
+#if defined(_WIN32) || defined(_WIN64)
+    return Type::Windows;
+#elif defined(__linux__)
+    return Type::Linux;
+#elif defined(__APPLE__) && defined(__MACH__)
+    return Type::MacOS;
+#else
+    return Type::Unknown;
+#endif
+}
+
+constexpr bool isWindows() { return currentPlatform() == Type::Windows; }
+
+constexpr bool isLinux() { return currentPlatform() == Type::Linux; }
+
+constexpr bool isMacOS() { return currentPlatform() == Type::MacOS; }
+
+} // namespace platform
 } // namespace info
 } // namespace vigine
